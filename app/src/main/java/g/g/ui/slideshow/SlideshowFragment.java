a88a0d6186c155afdea85f4ceafa55e99d6538b4
elosy <gaudic99@gmail.com>
2023-06-02 16:02:17 +0200

    tabs in SlideshowFragment (rankings)

--- app/src/main/java/g/g/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/g/g/ui/slideshow/SlideshowFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 
 import g.g.databinding.FragmentSlideshowBinding;
 
@@ -24,8 +28,21 @@ public class SlideshowFragment extends Fragment {
 		binding = FragmentSlideshowBinding.inflate(inflater, container, false);
 		View root = binding.getRoot();
 
-		final TextView textView = binding.textSlideshow;
-		slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+		// Get reference to the TabLayout and ViewPager2
+		TabLayout tabLayout = binding.tabLayout;
+		ViewPager2 viewPager = binding.viewPager;
+
+		// Create the adapter for ViewPager2
+		TabPagerAdapter pagerAdapter = new TabPagerAdapter(requireActivity());
+
+		// Set the adapter to the ViewPager2
+		viewPager.setAdapter(pagerAdapter);
+
+		// Link the TabLayout with the ViewPager2
+		new TabLayoutMediator(tabLayout, viewPager,
+				(tab, position) -> tab.setText("Tab " + (position + 1))
+		).attach();
+
 		return root;
 	}
 
